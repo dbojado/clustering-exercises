d6465fd96d5cda1543e6e62c546b8b85bfe4213e
Daniella Bojado <daniella.bojado@gmail.com>
2020-10-13 14:50:35 -0500

    add

--- acquire.py
+++ acquire.py
@@ -5,6 +5,39 @@ import seaborn as sns
 import os
 from env import host, username, password
 
+###################### Acquire Zillow Home Data ########################
+
+def get_connection(db, username=username, host=host, password=password):
+    '''
+    This function uses my info from my env file to
+    create a connection url to access the Codeup db.
+    '''
+    return f'mysql+pymysql://{username}:{password}@{host}/{db}'
+
+def new_zillow_data():
+    '''
+    This function reads the mall customer data from the Codeup db into a df, 
+    write it to a csv file, and returns the df.
+    '''
+    sql_query =  'SELECT * FROM properties_2017;'
+    df = pd.read_sql(sql_query, get_connection('zillow'))
+    df.to_csv('zillow_home.csv')
+    return df
+
+def get_zillow_data(cached=False):
+    '''
+    This function reads in mall customer data from Codeup database if cached == False
+    or if cached == True reads in mall customer df from a csv file, returns df
+    '''
+    if cached or os.path.isfile('zillow_home.csv') == False:
+        df = new_mall_data()
+    else:
+        df = pd.read_csv('zillow_home.csv', index_col=0)
+    return df
+
+
+
+
 ###################### Acquire Mall Customers Data #####################
 
 def get_connection(db, username=username, host=host, password=password):
