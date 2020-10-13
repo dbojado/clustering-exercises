9d266b0423d4d52790e676615d709f3b80be7760
Daniella Bojado <daniella.bojado@gmail.com>
2020-10-13 14:09:32 -0500

    add

--- prepare.py
+++ prepare.py
@@ -1,1 +1,30 @@
-#file
+import pandas as pd
+import numpy as np
+import scipy as sp 
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+
+from env import host, username, password
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, QuantileTransformer, PowerTransformer, RobustScaler, MinMaxScaler
+
+
+def data_prep(df, cols_to_remove=[], prop_required_column=.5, prop_required_row=.75): 
+    df = remove_columns(df, cols_to_remove)  # Removes Specified Columns
+    df = handle_missing_values(df, prop_required_column, prop_required_row) # Removes Specified Rows
+    df.dropna(inplace=True) # Drops all Null Values From Dataframe
+    return df
+
+
+def remove_columns(df, cols_to_remove):  
+    df = df.drop(columns=cols_to_remove)
+    return df
+
+def handle_missing_values(df, prop_required_column = .5, prop_required_row = .75):
+    threshold = int(round(prop_required_column*len(df.index),0))
+    df.dropna(axis=1, thresh=threshold, inplace=True)
+    threshold = int(round(prop_required_row*len(df.columns),0))
+    df.dropna(axis=0, thresh=threshold, inplace=True)
+    return df
